46a0b85863b3ef3a678bc8905fa84bf2116471ac
thristovska098 <78149781+thristovska098@users.noreply.github.com>
2023-05-10 20:42:51 +0200

    Update AddBook.jsx

--- bookapp/src/AddBook.jsx
+++ bookapp/src/AddBook.jsx
@@ -1,8 +1,80 @@
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+import { JsonForms } from '@jsonforms/react';
 
-const AddBook = () =>{
-// title, author, description, numberOfPages
-return <Button>Save book</Button>
+const uiSchema = {
+    "type": "VerticalLayout",
+    "elements": [
+        {
+        "type": "Control",
+        "scope": "#/properties/title"
+        },
+        {
+        "type": "Control",
+        "scope": "#/properties/author"
+        },
+        {
+          "type": "Control",
+          "scope": "#/properties/description"
+        },
+        {
+          "type": "Control",
+          "scope": "#/properties/numberOfPages"
+        }
+    ]
+}
+
+const schema = {
+    "type": "object",
+    "properties": {
+        "title": {
+        "type": "string",
+        "description": "Please enter the title of the book"
+        },
+        "author": {
+        "type": "string",
+        "description": "Please enter the name of the author"
+        },
+        "description": {
+        "type": "string",
+        "minLength": 3,
+        "description": "Please enter short summary of the book"
+        },
+        "numberOfPages": {
+        "type": "number"
+        }
+    }
+}
+
+const AddBook = () => {
+
+    const [data, setData] = useState();
+
+    const handleSaving = () => {
+        const requestOptions = {
+            method: 'POST',
+            body: data,
+        };
+
+        fetch('http://localhost:3005/book', requestOptions)
+            .then(response => console.log(response))
+    }
+
+    return <div style={{width:'550px'}} >
+             <JsonForms
+               schema={schema}
+               uischema={uiSchema}
+               data={data}
+               renderers={materialRenderers}
+               cells={materialCells}
+               onChange={({ data, errors }) => setData(data)}
+             />
+             <Button variant="contained" onClick={handleSaving}>Save</Button>
+           </div>
 }
 
 export default AddBook;
